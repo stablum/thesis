fixed gradients and removed some duplicated code

pmf_mu_hyperpriors.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 
 import movielens
 import cftools
+import config
 
 def wrong(x):
     return ((not np.isfinite(x)) or np.isnan(x) or x>10000. or x<-10000.)
@@ -17,21 +18,18 @@ def wrong(x):
 def main():
     np.set_printoptions(precision=4, suppress=True)
 
-    K=100
-    lr = 0.001
-    n_epochs = 1000
     lambda_v = 1.
     lambda_u = 1.
 
-    mu_u = np.random.random(K)*10. - 5.
-    mu_v = np.random.random(K)*10. - 5.
+    mu_u = np.random.random(config.K)*10. - 5.
+    mu_v = np.random.random(config.K)*10. - 5.
 
     R = movielens.small()
 
     N = R.shape[0]
     M = R.shape[1]
-    U = np.random.random((K,N))
-    V = np.random.random((K,M))
+    U = np.random.random((config.K,N))
+    V = np.random.random((config.K,M))
 
     def new_eij():
         ret = cftools.rating_error(Rij,U,i,V,j)
@@ -41,7 +39,7 @@ def main():
     training_set, testing_set = cftools.split_sets(R)
 
     print "training pmf with mu hyperpriors..."
-    for _ in tqdm(range(n_epochs)):
+    for _ in tqdm(range(config.n_epochs)):
         random.shuffle(training_set)
         for curr in tqdm(training_set):
             (i,j),Rij = curr
@@ -55,7 +53,7 @@ def main():
                 #    print "error introduced in eijv=%s"%(str(eijv))
                 #    import ipdb; ipdb.set_trace()
                 grad = eijv * U[:,i] + (1./N) * lambda_v * (V[:,j] - mu_v)
-                V[:,j] = V[:,j] + lr * grad
+                V[:,j] = V[:,j] + config.lr * grad
                 #if any([wrong(curr) for curr in V[:,j].tolist()]):
                 #    print "error introduced in V[:,%d]=%s"%(j,str(V[:,j]))
                 #    import ipdb; ipdb.set_trace()
@@ -66,18 +64,18 @@ def main():
                 #    print "error introduced in eiju=%s"%(str(eiju))
                 #    import ipdb; ipdb.set_trace()
                 grad = eiju * V[:,j] + (1./M) * lambda_u * (U[:,i] - mu_u)
-                U[:,i] = U[:,i] + lr * grad
+                U[:,i] = U[:,i] + config.lr * grad
                 #if any([wrong(curr) for curr in U[:,i].tolist()]):
                 #    print "error introduced in U[:,%d]=%s"%(i,str(U[:,i]))
                 #    import ipdb; ipdb.set_trace()
 
             elif toss == 3:
                 grad = lambda_v * (V[:,j] - mu_v)
-                mu_v = mu_v + lr * grad
+                mu_v = mu_v + config.lr * grad
 
             elif toss == 4:
                 grad = lambda_u * (U[:,i] - mu_u)
-                mu_u = mu_u + lr * grad
+                mu_u = mu_u + config.lr * grad
 
         print "mu_v",mu_v
         print "mu_u",mu_u

pmf_neural_hyperlatent.py
@@ -21,6 +21,8 @@ def main():
     sigma_v = 1.
     sigma_wu = 1.
     sigma_wv = 1.
+    sigma_hu = 1.
+    sigma_hv = 1.
 
     R = movielens.small()
 
@@ -45,25 +47,22 @@ def main():
         #print "eij",ret
         return ret
 
-    training_set, testing_set = cftools.split_sets(R)
-
     print "training pmf with hyperlatent neural vectors..."
-    for _ in tqdm(range(config.n_epochs)):
-        random.shuffle(training_set)
+    for training_set in cftools.epochsloop(R,U,V):
         for curr in tqdm(training_set):
             (i,j),Rij = curr
 
             neural_output_v = numutils.sigmoid(np.dot(Wv.T,Hv[:,j]))
             grad_neural = V[:,j] - neural_output_v
             eijv = new_eij()
-            grad = 1./sigma * eijv * U[:,i] + (sigma_v/N) * grad_neural
-            V[:,j] = V[:,j] + config.lr * grad
+            grad = (-1.)/sigma * eijv * U[:,i] + (1./(sigma_v*N)) * grad_neural
+            cftools.update(V[:,j],grad)
 
             neural_output_u = numutils.sigmoid(np.dot(Wu.T,Hu[:,i]))
             grad_neural = U[:,i] - neural_output_u
             eijv = new_eij()
-            grad = 1./sigma * eijv * V[:,j] + (sigma_u/M) * grad_neural
-            U[:,i] = U[:,i] + config.lr * grad
+            grad = (-1.)/sigma * eijv * V[:,j] + (1./(sigma_u*M)) * grad_neural
+            cftools.update(U[:,i],grad)
 
             sigmoid_deriv_v = (neural_output_v) * (1 - neural_output_v)
             neural_output_wv_grad = np.dot(
@@ -93,31 +92,28 @@ def main():
                 error_u = neural_output_u[k] - U[k,i]
 
                 f_prime = neural_output_wv_grad[k,:]
-                error_term = 1./sigma_v * error_v * f_prime
+                error_term = 1./sigma_v * error_v * f_prime * Hv[:,k]
                 prior_term = 1./sigma_wv * Wv[:,k]
                 grad = error_term + prior_term
-                Wv[:,k] = Wv[:,k] + config.lr * grad
+                cftools.update(Wv[:,k],grad)
 
                 f_prime = neural_output_wu_grad[k,:]
-                error_term = 1./sigma_u * error_u * f_prime
+                error_term = 1./sigma_u * error_u * f_prime * Hu[:,k]
                 prior_term = 1./sigma_wu * Wu[:,k]
                 grad = error_term + prior_term
-                Wu[:,k] = Wu[:,k] + config.lr * grad
+                cftools.update(Wu[:,k],grad)
 
                 f_prime = neural_output_hv_grad
-                error_term = 1./sigma_v * error_v * f_prime
-                prior_term = 1./sigma_wv * Wv[:,k]
+                error_term = 1./sigma_v * error_v * f_prime * Wv[:,k]
+                prior_term = 1./sigma_hv * Hv[:,k]
                 grad = error_term + prior_term
-                Hv[:,k] = Hv[:,k] + config.lr * grad
+                cftools.update(Hv[:,k],grad)
 
                 f_prime = neural_output_hu_grad
-                error_term = 1./sigma_u * error_u * f_prime
-                prior_term = 1./sigma_wu * Wu[:,k]
+                error_term = 1./sigma_u * error_u * f_prime * Wu[:,k]
+                prior_term = 1./sigma_hu * Hu[:,k]
                 grad = error_term + prior_term
-                Hu[:,k] = Hu[:,k] + config.lr * grad
-
-        print "training RMSE: ",cftools.rmse(training_set,U,V)
-        print "testing RMSE: ",cftools.rmse(testing_set,U,V)
+                cftools.update(Hu[:,k],grad)
 
 if __name__=="__main__":
     main()