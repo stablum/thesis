d56c75ed475058339b14e8a4252a11f1f30701f3
Francesco Stablum <stablum@gmail.com>
2016-10-05 10:26:53 +0200

    fixed engage.sh

--- engage.sh
+++ engage.sh
@@ -11,5 +11,5 @@ read COMMIT_MESSAGE
 git commit -m "$COMMIT_MESSAGE"
 git push das4vu master
 echo "after submitting the job will sleep for $SLEEPTIME seconds before retrieving outputs"
-$SSH "cd reimplementations; git stash; qstat | grep fstablum ; qsub -o $TIMESTAMP.out -e $TIMESTAMP.err -cwd -l gpu=C2050 -l h_rt=24:00:00 job.sh -- $@ ; sleep $SLEEPTIME; tail -f $TIMESTAMP.out & tail -f $TIMESTAMP.err "
+$SSH "cd thesis; git stash; qstat | grep fstablum ; qsub -o $TIMESTAMP.out -e $TIMESTAMP.err -cwd -l gpu=C2050 -l h_rt=24:00:00 job.sh -- $@ ; sleep $SLEEPTIME; tail -f $TIMESTAMP.out & tail -f $TIMESTAMP.err "
  
