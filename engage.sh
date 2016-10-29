bdcf56ada09b815088a1efbb6db581a5516e1047
Francesco Stablum <stablum@gmail.com>
2016-10-29 15:40:50 +0200

    with dropout

--- engage.sh
+++ engage.sh
@@ -13,9 +13,9 @@ read COMMIT_MESSAGE
 git commit -m "$COMMIT_MESSAGE"
 git push das4vu master
 #NODETYPE="gpu=GTX680"
-NODETYPE="fat,gpu=K20"
+#NODETYPE="fat,gpu=K20"
 #NODETYPE="gpu=C2050"
-#NODETYPE="gpu=GTX480"
+NODETYPE="gpu=GTX480"
 echo "after submitting the job will sleep for $SLEEPTIME seconds before retrieving outputs"
 $SSH "quota -m; cd thesis; git stash; qstat | grep fstablum ; qsub -N '$@' -o $TIMESTAMP.out -e $TIMESTAMP.err -cwd -l $NODETYPE -l h_rt=96:00:00 job.sh -- $@ ; sleep $SLEEPTIME; tail -f $TIMESTAMP.out & tail -f $TIMESTAMP.err "
  
