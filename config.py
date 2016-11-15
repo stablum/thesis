90f6c36a8ba8b335148b0e19838cf9c6ad22dc42
Francesco Stablum <stablum@gmail.com>
2016-11-15 17:54:00 +0100

    slower learning rate

--- config.py
+++ config.py
@@ -17,13 +17,13 @@ theano.mode = theano_mode
 n_epochs=5000
 
 K=10
-hid_dim=2000
+hid_dim=20
 n_hid_layers=1
 chan_out_dim=K
 stochastic_prediction=False#True
 regularization_lambda=0#0.01
 dropout_p=0#0.5
-lr_begin=0.001#0.00001#0.000001#0.5#0.005
+lr_begin=0.00001#0.00001#0.000001#0.5#0.005
 lr_annealing_T=n_epochs
 max_rating=5.
 
