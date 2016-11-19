4c107f34a576c56f8abd031f6bf73110dc505bcd
Francesco Stablum <stablum@gmail.com>
2016-11-19 13:41:10 +0100

    gtx680

--- engage.sh
+++ engage.sh
@@ -13,8 +13,8 @@ read COMMIT_MESSAGE
 git commit -m "$COMMIT_MESSAGE"
 git push das4vu master
 #NODETYPE="ngpus=1"
-#NODETYPE="gpu=GTX680"
-NODETYPE="fat,gpu=K20"
+NODETYPE="gpu=GTX680"
+#NODETYPE="fat,gpu=K20"
 #NODETYPE="gpu=C2050"
 #NODETYPE="gpu=GTX480"
 echo "NODETYPE:$NODETYPE"
