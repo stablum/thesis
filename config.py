677e092954192bc6def07722fe654412667ef930
Francesco Stablum <stablum@gmail.com>
2016-11-22 22:37:56 +0100

    lr 1e-6

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ regularization_lambda=1e-3
 regularization_latent_kl=0.5
 input_dropout_p=0.5
 dropout_p=0.1
-lr_begin=1e-7 # 1e-5 # 1e-6 # 0.5 # 5e-3
+lr_begin=1e-6 # 1e-5 # 1e-6 # 0.5 # 5e-3
 lr_annealing_T=n_epochs
 max_rating=5.
 
