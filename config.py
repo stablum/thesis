107b0f9f384fb8c463d734a938546af32394bb82
Francesco Stablum <stablum@gmail.com>
2016-11-26 22:08:35 +0100

    k=14

--- config.py
+++ config.py
@@ -17,14 +17,14 @@ theano.mode = theano_mode
 
 n_epochs=20000
 
-K=15
+K=14
 hid_dim=50
 n_hid_layers=0
 chan_out_dim=K
 stochastic_prediction=False#True
 regularization_lambda=1e-3
 regularization_latent_kl=0.5
-input_dropout_p=0.5
+input_dropout_p=0.1
 dropout_p=0.1
 lr_begin=2e-6 # 1e-5 # 1e-6 # 0.5 # 5e-3
 lr_annealing_T=n_epochs
