d257c89d2c4fc38003f9ddfdbf62a25f26b3f874
Francesco Stablum <stablum@gmail.com>
2017-01-07 11:25:23 +0100

    gtx680

--- engage.sh
+++ engage.sh
@@ -13,10 +13,10 @@ read COMMIT_MESSAGE
 git commit -m "$COMMIT_MESSAGE"
 git push das4vu master
 #NODETYPE="ngpus=1"
-#NODETYPE="gpu=GTX680"
+NODETYPE="gpu=GTX680"
 #NODETYPE="fat,gpu=K20"
 #NODETYPE="gpu=C2050"
-NODETYPE="gpu=GTX480"
+#NODETYPE="gpu=GTX480"
 echo "NODETYPE:$NODETYPE"
 echo "after submitting the job will sleep for $SLEEPTIME seconds before retrieving outputs"
 $SSH "quota -m; cd thesis; git stash; qstat | grep fstablum ; qsub -N '$@' -o $TIMESTAMP.out -e $TIMESTAMP.err -cwd -l $NODETYPE -l h_rt=43:00:00 job.sh -- $@ ; sleep $SLEEPTIME; tail -f $TIMESTAMP.out & tail -f $TIMESTAMP.err "
