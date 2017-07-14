1e90e074ba898057469e6cbb6f625c50c2aa5026
Francesco Stablum <stablum@gmail.com>
2017-07-14 17:45:29 +0200

    user+item

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ preprocessing_type = "vanilla" # in 0to1/vanilla/zscore
 chunk_len =64*1024
 minibatch_size = 16 #2 # 16 # 64
 
-regression_type = "item" # in user/item/user+item
+regression_type = "user+item" # in user/item/user+item
 
 if socket.gethostname() in ['playertrackingmobile']:
     # locally
