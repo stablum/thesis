5e1705046119117762cd83630659ac01b4c69add
Francesco Stablum <stablum@gmail.com>
2017-10-08 17:37:16 +0200

    autorec reg l2 1e-3

--- engage.sh
+++ engage.sh
@@ -12,9 +12,9 @@ echo -n "commit message:"
 read COMMIT_MESSAGE
 git commit -m "$COMMIT_MESSAGE"
 git push das4vu master
-NODETYPE="ngpus=1"
+#NODETYPE="ngpus=1"
 #NODETYPE="gpu=GTX680"
-#NODETYPE="fat,gpu=K20"
+NODETYPE="fat,gpu=K20"
 #NODETYPE="gpu=C2050"
 #NODETYPE="gpu=GTX480"
 echo "NODETYPE:$NODETYPE"
