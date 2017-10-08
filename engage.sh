8922aa4864851820eb2ede1ded02e2ee09fa1c4b
Francesco Stablum <stablum@gmail.com>
2017-10-08 15:37:39 +0200

    from 7 to 5 days

--- engage.sh
+++ engage.sh
@@ -20,5 +20,5 @@ NODETYPE="gpu=GTX480"
 echo "NODETYPE:$NODETYPE"
 echo "TIMESTAMP:$TIMESTAMP"
 echo "after submitting the job will sleep for $SLEEPTIME seconds before retrieving outputs"
-$SSH "quota -m; cd thesis; git stash; qstat | grep fstablum ; qsub -N '$@' -o $TIMESTAMP.out -e $TIMESTAMP.err -cwd -l $NODETYPE -l h_rt=168:00:00 job.sh -- $@ ; sleep $SLEEPTIME; tail -f $TIMESTAMP.out & tail -f $TIMESTAMP.err "
+$SSH "quota -m; cd thesis; git stash; qstat | grep fstablum ; qsub -N '$@' -o $TIMESTAMP.out -e $TIMESTAMP.err -cwd -l $NODETYPE -l h_rt=120:00:00 job.sh -- $@ ; sleep $SLEEPTIME; tail -f $TIMESTAMP.out & tail -f $TIMESTAMP.err "
  
