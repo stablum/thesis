26574c82b1e5e4c24f17657f9537103cc315ca70
Francesco Stablum <stablum@gmail.com>
2018-01-02 18:25:43 +0100

    lambda 0

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ hid_dim=500
 n_hid_layers=1
 chan_out_dim=K
 stochastic_prediction=False#True
-regularization_lambda=2e+2
+regularization_lambda=0.0#2e+2
 regularization_type="L2" # in L1/L2
 regularization_latent_kl=0.5#0#0.5
 input_dropout_p=0.0
 dropout_p=0.0
-lr_begin=2e-7#2e-6 # 1e-5 # 1e-6 # 0.5 # 5e-3
+lr_begin=2e-5#2e-6 # 1e-5 # 1e-6 # 0.5 # 5e-3
 lr_annealing_T=n_epochs
 max_rating=5.
 
