dfd589aca9b86183ad06893a69dceef88c39bcb9
Francesco Stablum <stablum@gmail.com>
2018-03-24 11:44:59 +0100

    working on the resume job script

--- resume_jobs.sh
+++ resume_jobs.sh
@@ -5,5 +5,8 @@ bash sync_src.sh
 
 pushd ~/das4mount
 cd thesis
-python3.6m list_harvest_with_state.py
+python3.6m list_harvest_with_state.py --automatic-max-epoch | while read HARVEST_DIR ; do
+    echo resuming job on $HARVEST_DIR ...
+
+done
 popd
