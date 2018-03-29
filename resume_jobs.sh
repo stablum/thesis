e2173322ddae90c40495543b2cd36a722bca2144
Francesco Stablum <stablum@gmail.com>
2018-03-29 15:28:34 +0200

    better red echo

--- resume_jobs.sh
+++ resume_jobs.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-echo "mount sftp.."
+function myecho {
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+    echo -e "$RED$@$NC"
+}
+
+myecho "mount sftp.."
 bash mount_sftp.sh
 
-echo "sync src.."
+myecho "sync src.."
 bash sync_src.sh
 
-echo "pushd in the das4mount.."
+myecho "pushd in the das4mount.."
 pushd ~/das4mount
 cd thesis
 
 SLEEPTIME=10
-RED='\033[0;31m'
-NC='\033[0m' # No Color
 HARVESTS=$(python3.6m list_harvest_with_state.py --automatic-max-epoch)
-TOTAL=$(echo $HARVESTS | wc -l)
-echo -e "$RED HARVESTS=$HARVESTS"
-echo -e "$RED looping on the harvest dirs.. (pwd=$(pwd)) $NC"
+TOTAL=$(myecho $HARVESTS | wc -w)
+myecho "HARVESTS=$HARVESTS"
+myecho "looping on the harvest dirs.. (pwd=$(pwd))"
 COUNTER=0
 for HARVEST_DIR in $HARVESTS ; do
     let COUNTER=COUNTER+1
-    echo -e "$RED ($COUNTER/$TOTAL) resuming job on $HARVEST_DIR ... $NC"
+    myecho "($COUNTER/$TOTAL) resuming job on $HARVEST_DIR ..."
     bash engage.sh resume_one.sh $HARVEST_DIR &
-    echo -e "$RED ($COUNTER/$TOTAL) sleeping $SLEEPTIME ... $NC"
+    myecho "($COUNTER/$TOTAL) sleeping $SLEEPTIME ... "
     sleep $SLEEPTIME
 done
 popd
