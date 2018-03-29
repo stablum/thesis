ada4647495c091d66cedb90a4e3cee3e6764fd26
Francesco Stablum <stablum@gmail.com>
2018-03-29 15:24:53 +0200

    total

--- resume_jobs.sh
+++ resume_jobs.sh
@@ -14,14 +14,15 @@ SLEEPTIME=10
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 HARVESTS=$(python3.6m list_harvest_with_state.py --automatic-max-epoch)
+TOTAL=$(echo $HARVESTS | wc -l)
 echo -e "$RED HARVESTS=$HARVESTS"
 echo -e "$RED looping on the harvest dirs.. (pwd=$(pwd)) $NC"
 COUNTER=0
 for HARVEST_DIR in $HARVESTS ; do
     let COUNTER=COUNTER+1
-    echo -e "$RED ($COUNTER) resuming job on $HARVEST_DIR ... $NC"
+    echo -e "$RED ($COUNTER/$TOTAL) resuming job on $HARVEST_DIR ... $NC"
     bash engage.sh resume_one.sh $HARVEST_DIR &
-    echo -e "$RED ($COUNTER) sleeping $SLEEPTIME ... $NC"
+    echo -e "$RED ($COUNTER/$TOTAL) sleeping $SLEEPTIME ... $NC"
     sleep $SLEEPTIME
 done
 popd
