b155cea93b176dc7bc8213969bcc136e38447d58
Francesco Stablum <stablum@gmail.com>
2018-03-29 14:46:04 +0200

    remove ask for commit message if nothing to commit

--- sync_src.sh
+++ sync_src.sh
@@ -3,13 +3,17 @@
 git add -u
 git add $1
 git diff --cached
-echo -n "commit message:"
-read COMMIT_MESSAGE
-git commit -m "$COMMIT_MESSAGE"
-bash mount_sftp.sh
-git push das4mount master
+if test $(git diff --cached | wc -l) -ne 0 ; then
+    echo -n "commit message:"
+    read COMMIT_MESSAGE
+    git commit -m "$COMMIT_MESSAGE"
+    bash mount_sftp.sh
+    git push das4mount master
+else
+    echo "nothing to send to remote repository"
+fi
+
 pushd ~/das4mount/
 cd thesis
 git stash
 popd
-
