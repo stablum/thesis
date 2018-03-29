9bca67a42045f2bbd9dd4eff9ff3cd0d77c8c1a0
Francesco Stablum <stablum@gmail.com>
2018-03-29 15:13:00 +0200

    debug gather harvests

--- resume_jobs.sh
+++ resume_jobs.sh
@@ -13,10 +13,12 @@ cd thesis
 SLEEPTIME=10
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+HARVESTS=$(python3.6m list_harvest_with_state.py --automatic-max-epoch)
+echo -e "$RED HARVESTS=$HARVESTS"
 echo -e "$RED looping on the harvest dirs.. (pwd=$(pwd)) $NC"
-python3.6m list_harvest_with_state.py --automatic-max-epoch | while read HARVEST_DIR ; do
+for HARVEST_DIR in $HARVESTS ; do
     echo -e "$RED resuming job on $HARVEST_DIR ... $NC"
-    bash engage.sh resume_one.sh $HARVEST_DIR &
+    #bash engage.sh resume_one.sh $HARVEST_DIR &
     echo -e "$RED sleeping $SLEEPTIME ... $NC"
     sleep $SLEEPTIME
 done
