e1b6d664c8d08b950f78ba7225758c226a9b396a
Francesco Stablum <stablum@gmail.com>
2018-04-15 12:53:04 +0200

    fixed mount_sftp

--- mount_sftp.sh
+++ mount_sftp.sh
@@ -2,6 +2,17 @@
 
 KEY=~/.ssh/das4vu
 HOST=fs0.das4.cs.vu.nl
+MOUNTPOINT=~/das4mount
 ssh-add $KEY
-sshfs fstablum@$HOST:/home/fstablum ~/das4mount
+if test $(mount | grep $(basename $MOUNTPOINT) | wc -l) -eq 1 ; then
+    echo "unmounting das4 mount.."
+    sudo umount $MOUNTPOINT
+    echo "unmounting done."
+fi
+sshfs fstablum@$HOST:/home/fstablum $MOUNTPOINT
+
+if test $(ls $MOUNTPOINT | wc -l ) -eq 0 ; then
+    echo "WARNING!! unable to mount on $MOUNTPOINT !!!!!!!"
+    exit
+fi
 
--- retrieve_harvest.sh
+++ retrieve_harvest.sh
@@ -3,8 +3,14 @@
 KEY=~/.ssh/das4vu
 HOST=fs0.das4.cs.vu.nl
 
+bash mount_sftp.sh
+
 bash list_harvest.sh | while read x ; do
-    scp -r -v -i $KEY fstablum@$HOST:thesis/$x .
+    echo "retrieve_harvest: processing directory $x ...."
+    echo "copying.."
+    cp -r ~/das4mount/thesis/$x .
+    echo "copying done. making plots.."
     bash make_png_plots_in_dir.sh $x
+    echo "retrieve_harvest: processing directory $x done."
 done
 
