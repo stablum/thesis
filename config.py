4be67751c91aaa5b6a63383f2622a9e2985d0a89
Francesco Stablum <stablum@gmail.com>
2018-05-20 11:44:26 +0200

    autorec mb64

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ seed=12345
 n_epochs=1000
 
 K=250
-TK=0# transformation's K (number of nested transformation steps)
+TK=20# transformation's K (number of nested transformation steps)
 hid_dim=1000
-n_hid_layers=2
+n_hid_layers=1
 chan_out_dim=K
 stochastic_prediction=False#True
 regularization_lambda=2e+2
 regularization_type="L2" # in L1/L2
-regularization_latent_kl=0.0#0#0.5
+regularization_latent_kl=0.5#0#0.5
 input_dropout_p=0.0
 dropout_p=0.0
 batch_normalization=False
@@ -53,7 +53,7 @@ preprocessing_type = "vanilla" # in 0to1/vanilla/zscore
 spherical_likelihood = True
 
 chunk_len =64*1024
-minibatch_size = 64 #1 #2 # 16 # 64
+minibatch_size = 64#2 # 16 # 64
 
 regression_type = "item" # in user/item/user+item
 
