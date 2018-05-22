43da22a4f6c302fcfd724954eac708a26432d300
Francesco Stablum <stablum@gmail.com>
2018-05-22 16:31:07 +0200

    tk

--- summarize_harvest.sh
+++ summarize_harvest.sh
@@ -6,5 +6,5 @@ echo HARVESTS: $HARVESTS
 python3 logs_summarizer.py $HARVESTS \
     -f "" \
     -s best_testing_rmse \
-    -c lr,best_testing_rmse,best_training_rmse,minibatch_size,regularization_latent_kl,regularization_type,regularization_lambda,n_hid_layers,hid_dim,K,harvest_dir,max_epoch,upd,g_hid,regression_type,nan,dropout_p,input_dropout_p
+    -c lr,best_testing_rmse,best_training_rmse,minibatch_size,regularization_latent_kl,regularization_type,regularization_lambda,n_hid_layers,hid_dim,K,harvest_dir,max_epoch,upd,g_hid,regression_type,nan,dropout_p,input_dropout_p,TK
     #--twod=lr,regularization_lambda
