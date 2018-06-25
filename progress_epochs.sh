51b4cb171b04721147bbd88d0dfe6f14f24345dd
Francesco Stablum <stablum@gmail.com>
2018-06-25 12:30:01 +0200

    new progress script

--- progress_epochs.sh
+++ progress_epochs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 list_harvest_with_state.py | while read x ; do echo -n "$x " ; cat $x/*.log | grep training | wc -l ; done
+python3 list_harvest_with_state.py | while read x ; do echo -n "$x " ; cat $x/*.log | grep epoch | wc -l ; done
