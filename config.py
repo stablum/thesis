2a0506be6c4835a5d03ea3e0d498c466be8ed46f
Francesco Stablum <stablum@gmail.com>
2018-07-02 01:38:24 +0200

    T=10 on autorec comparer

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ hid_dim=1000
 n_hid_layers=1
 chan_out_dim=K
 stochastic_prediction=False#True
-regularization_lambda=0 #2e+2
+regularization_lambda=2e+2
 regularization_type="L2" # in L1/L2
-regularization_latent_kl=1.0#0#0.5
+regularization_latent_kl=0.0#0#0.5
 input_dropout_p=0.0
 dropout_p=0.0
 batch_normalization=False
@@ -30,7 +30,6 @@ ratings_training_set_subsample_size = 5000
 
 regression_error_coef=0.5#1.#100.#1.#2.
 
-#update_algorithm = 'rprop_masked'
 update_algorithm = 'adam_masked'
 #update_algorithm = 'sgd'
 
@@ -54,7 +53,7 @@ preprocessing_type = "vanilla" # in 0to1/vanilla/zscore
 spherical_likelihood = True
 
 chunk_len =64*1024
-minibatch_size = 64 #1 #2 # 16 # 64
+minibatch_size = 64 # 1 #2 # 16 # 64
 
 regression_type = "item" # in user/item/user+item
 
@@ -70,8 +69,7 @@ if debug:
     optimizer = "debug"
 else:
     movielens_which='1m'
-    optimizer = "debug"
-    #optimizer = "gpu_omp"
+    optimizer = "gpu_omp"
 
 if optimizer == "debug":
     theano.config.exception_verbosity="high"
