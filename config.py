9c65e4916a8f9a9b4110813934bd7ddaa4296388
Francesco Stablum <stablum@gmail.com>
2018-08-05 18:11:11 +0200

    user+item TK=5

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ seed=12345
 n_epochs=1000
 
 K=5
-TK=1 # transformation's K (number of nested transformation steps)
+TK=5 # transformation's K (number of nested transformation steps)
 enforce_invertibility=True
 flow_type="realnvp"
 g_flow="tanh"
@@ -62,7 +62,7 @@ spherical_likelihood = True
 chunk_len =64*1024
 minibatch_size = 64 #1 #2 # 16 # 64
 
-regression_type = "user" # in user/item/user+item
+regression_type = "user+item" # in user/item/user+item
 
 nanguard=False
 verbose=False
