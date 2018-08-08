bf8b2f6710d183229974b01c5031fe9ece37192c
Francesco Stablum <stablum@gmail.com>
2018-08-08 21:16:03 +0200

    txt, and the amazing

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ import movielens
 seed=12345
 n_epochs=1000
 
-K=5
-TK=1 # transformation's K (number of nested transformation steps)
+K=250
+TK=0 # transformation's K (number of nested transformation steps)
 enforce_invertibility=True
 flow_type="realnvp"
 g_flow="tanh"
 hid_dim=1000
-n_hid_layers=1
+n_hid_layers=2
 chan_out_dim=K
 stochastic_prediction=False#True
 regularization_lambda=100
@@ -62,7 +62,7 @@ spherical_likelihood = True
 chunk_len =64*1024
 minibatch_size = 64 #1 #2 # 16 # 64
 
-regression_type = "user" # in user/item/user+item
+regression_type = "item" # in user/item/user+item
 
 nanguard=False
 verbose=False
